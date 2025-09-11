f2054ae11366d619e1533c74591024aa36d638e3
Teacher Isaac <teacher@eduk8.us>
2025-09-11 15:05:14 +0700

    specify whole number output

--- topic-1.5/TemperatureConverter.java
+++ topic-1.5/TemperatureConverter.java
@@ -14,7 +14,7 @@ public class TemperatureConverter {
         
         // Students complete:
         // 1. Convert to Fahrenheit (exact)
-        // 2. Round to nearest degree
+        // 2. Round to nearest degree (print as a whole number)
         // 3. Display both results
         
         input.close();
